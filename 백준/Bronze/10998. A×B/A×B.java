830fb05058530e50fc0e287f4f5349396b9e23a8
geonwoolee95 <144126452+geonwoolee95@users.noreply.github.com>
2024-11-20 17:20:24 +0900

    [Bronze V] Title: A×B, Time: 172 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		int A;
+		int B;
+		int x; // A-B를 곱하는 값
+		
+		
+		Scanner scan = new Scanner(System.in);
+		A = scan.nextInt();
+		B = scan.nextInt();
+		x = A*B;
+		System.out.print(x);
+		
+		
+	}
+
+}
