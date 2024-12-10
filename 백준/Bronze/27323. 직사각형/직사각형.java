30caa26a78086c1e73de1c34ac7491e753a816ac
geonwoolee95 <144126452+geonwoolee95@users.noreply.github.com>
2024-12-10 17:30:44 +0900

    [Bronze V] Title: 직사각형, Time: 176 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+
+		int a ;
+		int b ;
+		
+		Scanner scan = new Scanner(System.in);
+		
+		a = scan.nextInt();
+		b = scan.nextInt();
+		
+		System.out.println(a*b);
+
+	}
+
+}
